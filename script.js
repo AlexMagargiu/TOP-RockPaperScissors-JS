c3957d217ce51feabe128d885daf415fa6621b55
AlexMagargiu <122966645+AlexMagargiu@users.noreply.github.com>
2023-03-09 13:07:49 +0200

    Create the ability to play a round in the browsers console.

--- script.js
+++ script.js
@@ -2,10 +2,33 @@
 function getComputerChoice(){
     const computerChoice = Math.floor(Math.random() * 1000)
     if (computerChoice % 3 === 0){
-        return "rock";
+        return "Rock";
     } else if (computerChoice % 3 === 1){
-        return "paper";
+        return "Paper";
     } else {
-        return "scissors";
+        return "Scissors";
     }
 }
+
+//Compares the choice of the player with the random one from the computer
+//and returns the one that a message for the one that won.
+function playRound(playerSelection, computerSelection){
+    playerSelection = playerSelection.toLowerCase();
+    computerSelection = getComputerChoice().toLowerCase();
+    if(playerSelection === computerSelection){
+        return "It's a tie"
+    } else if (
+        (playerSelection == "rock" && computerSelection == "paper") ||
+        (playerSelection == "paper" && computerSelection == "scissors") ||
+        (playerSelection == "scissors" && computerSelection == "rock")
+    ){
+        return "You lose this round!";
+    } else {
+        return "You win this round!";
+    }
+}
+
+//For testing to see if everything works
+const playerSelection = prompt("Rock, paper or scissors?", "rock");
+const computerSelection = getComputerChoice();
+console.log(playRound(playerSelection, computerSelection));
